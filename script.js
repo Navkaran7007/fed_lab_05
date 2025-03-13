be0efec4498b607b22b47b0f3c2114c87aa5fbc0
Navkaran Singh Sidhu <nsidhu10@academic.rrc.ca>
2025-03-13 09:46:08 -0500

    fix function error

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-async function simulateAsyncOperation() {
+function simulateAsyncOperation() {
 	return new Promise((resolve) =>
 		setTimeout(() => {
 			resolve("Data fetched after waiting");
@@ -23,6 +23,24 @@ fetchCatBreeds().then((response) => console.log(response));
 
 // Fetch and display cat facts
 const factsContainer = document.getElementById("factsContainer");
+fetch("https://catfact.ninja/facts?limit=10")
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error, status = ${response.status}`);
+        }
+        return response.json();
+    })
+    .then((data) => {
+        for (const fact of data.data) {
+            const factElement = document.createElement("p");
+            factElement.textContent = fact.fact;
+            factsContainer.appendChild(factElement);
+        }
+    })
+    .catch((error) => {
+        console.error("Error fetching cat facts:", error);
+    });
+    
 
 const samuraiPizzaCats = {
     leader: "Speedy Cerviche",
